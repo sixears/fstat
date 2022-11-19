ada4b626469cfc5b477c10c3246c96a81efcfe2f
Mr. Fluffy <git@sixearsm>
2022-11-19 11:06:48 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Haskell version of C's struct stat";
 
   inputs = {
-    nixpkgs.url      = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
+    nixpkgs.url      = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
     tfmt.url         = github:sixears/tfmt/r0.2.7.23;
@@ -11,9 +11,20 @@
   outputs = { self, nixpkgs, build-utils
             , tfmt }:
     build-utils.lib.hOutputs self nixpkgs "fstat" {
-      deps = {
-        inherit tfmt;
-      };
-      ghc = p: p.ghc8107; # for tfmt
+      ghc = p: p.ghc8107; # for tfmtoptparse-plus
+      callPackage = { mkDerivation, lib, mapPkg, system
+                    , base, base-unicode-symbols, data-textual, text
+                    , text-printer, time, unix
+                    }:
+        mkDerivation {
+          pname = "fstat";
+          version = "1.0.2.24";
+          src = ./.;
+          libraryHaskellDepends = [
+            base base-unicode-symbols data-textual text text-printer time unix
+          ] ++ mapPkg [ tfmt ];
+          description = "Haskell version of C's struct stat";
+          license = lib.licenses.mit;
+        };
     };
 }
