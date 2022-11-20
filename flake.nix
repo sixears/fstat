d2a82bcbee73300783141d64a5cdbb37af1d4677
Mr. Fluffy <git@sixearsm>
2022-11-20 06:53:13 +0000

    exited->1.0.4.22;monaderror-io->1.2.5.19;tfmt->0.2.7.24;number->1.1.2.14;has-callstack->1.0.1.18;base0->0.0.4.11;more-unicode->0.0.17.12;quasiquoting->1.0.1.31

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     nixpkgs.url      = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url  = github:sixears/flake-build-utils/r1.0.0.13;
 
-    tfmt.url         = github:sixears/tfmt/r0.2.7.23;
+    tfmt.url         = github:sixears/tfmt/r0.2.7.24;
   };
 
   outputs = { self, nixpkgs, build-utils
@@ -18,7 +18,7 @@
                     }:
         mkDerivation {
           pname = "fstat";
-          version = "1.0.2.24";
+          version = "1.0.2.25";
           src = ./.;
           libraryHaskellDepends = [
             base base-unicode-symbols data-textual text text-printer time unix
